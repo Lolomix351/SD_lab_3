f45f31fe66b622097473b83c600e9c50cb13e689
CoBaKa351 <dmitrishakyla@gmail.com>
2025-03-02 12:33:53 +0300

    Initial commit

---
+++ exercise_2.py
@@ -0,0 +1,18 @@
+# Шакула Дмитрий Андреевич 090301-ПОВа-о24
+
+def gcd(x, y):
+    while y != 0:
+        x, y = y, x % y
+    return x
+
+# Считываем входные данные
+a, b, c = map(int, input().split())
+
+# Находим НОД(a, b)
+d = gcd(a, b)
+
+# Проверяем, делит ли НОД число c
+if c % d == 0:
+    print(d)
+else:
+    print("Impossible")
